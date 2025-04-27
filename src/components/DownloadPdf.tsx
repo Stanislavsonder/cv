ae4a92b3feaf8865ff638fe7a71c0c968455b4c1
Stanislavsonder <stanislavsonder@gmail.com>
2025-04-27 15:45:11 -0400

    Add loading state and styles to PDF download button
    Enhanced the DownloadPdfButton component to include a loading state while the PDF is being generated. Added corresponding styles for a loader animation and disabled the button during the loading process. Improved error handling during the download process.

--- src/components/DownloadPdf.tsx
+++ src/components/DownloadPdf.tsx
@@ -1,34 +1,52 @@
 'use client'
 
+import { useState } from 'react'
 import styles from './DownloadPdf.module.css'
 
 export default function DownloadPdfButton() {
+	const [isLoading, setIsLoading] = useState(false)
+
 	const handleDownload = async () => {
-		const res = await fetch('/api/generate-pdf')
-		const blob = await res.blob()
-		const url = window.URL.createObjectURL(blob)
+		try {
+			setIsLoading(true)
+			const res = await fetch('/api/generate-pdf')
+			const blob = await res.blob()
+			const url = window.URL.createObjectURL(blob)
 
-		const currentDate = new Date()
-		const formattedDate = currentDate.toLocaleDateString('en-US', {
-			year: 'numeric',
-			month: '2-digit',
-			day: '2-digit'
-		})
-		const filename = `Stanislav_Doronin_CV (${formattedDate}).pdf`
+			const currentDate = new Date()
+			const formattedDate = currentDate.toLocaleDateString('en-US', {
+				year: 'numeric',
+				month: '2-digit',
+				day: '2-digit'
+			})
+			const filename = `Stanislav_Doronin_CV (${formattedDate}).pdf`
 
-		const a = document.createElement('a')
-		a.href = url
-		a.download = filename
-		a.click()
-		window.URL.revokeObjectURL(url)
+			const a = document.createElement('a')
+			a.href = url
+			a.download = filename
+			a.click()
+			window.URL.revokeObjectURL(url)
+		} catch (error) {
+			console.error('Error downloading PDF:', error)
+		} finally {
+			setIsLoading(false)
+		}
 	}
 
 	return (
 		<button
 			className={styles.linkButton}
 			onClick={handleDownload}
+			disabled={isLoading}
 		>
-			Download PDF
+			{isLoading ? (
+				<>
+					<span className={styles.loader}></span>
+					<span style={{ marginLeft: '8px' }}>Generating PDF...</span>
+				</>
+			) : (
+				'Download PDF'
+			)}
 		</button>
 	)
 }
